695da8b42eed8866e531d5fd274331e511a8cb4b
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-01-30 18:41:57 +0500

    123

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link, useLocation, NavLink } from 'react-router-dom'
+
+export default function Header({ loggedIn, email, signOut }) {
+  const location = useLocation()
+
+  return (
+    <header className="header">
+      <Link to="." className="header__link">
+        <img
+          src={require('../images/logo')}
+          alt="logo"
+          className="header__logo"
+        ></img>
+      </Link>
+
+      <ul className="header__navbar">
+        <li className="header__navbar-item">
+          <NavLink to="/movies" className="header__navbar-item_type_link">
+            Фильмы
+          </NavLink>
+        </li>
+        <li className="header__navbar-item">
+          <NavLink to="/movies" className="header__navbar-item_type_link">
+            Сохраненные фильмы
+          </NavLink>
+        </li>
+      </ul>
+
+      <Link to='profile' className='header__link-profile'>Аккаунт</Link>
+    </header>
+  )
+}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+
+export default function Main({}) {
+  
+}
