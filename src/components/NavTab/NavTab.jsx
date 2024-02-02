fffd03290459ffd70c98fd7214e650053124c7d2
psychoHAHA <80909413+psychoHAHA@users.noreply.github.com>
2024-02-02 20:34:18 +0300

    доделать технологии

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -4,8 +4,8 @@ export default function NavTab() {
   return (
     <>
       <nav className="nav">
-        <a href="#" className="nav__link">О проекте</a>
-        <a href="#" className="nav__link">Технологии</a>
+        <a href="#AboutProject" className="nav__link">О проекте</a>
+        <a href="#Techs" className="nav__link">Технологии</a>
         <a href="#" className="nav__link">Студент</a>
       </nav>
     </>
