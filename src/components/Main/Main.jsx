fbea74e9eae1934c74f37ee5f807db1c6409f755
psychoHAHA <80909413+psychoHAHA@users.noreply.github.com>
2024-02-02 20:34:18 +0300

    доделать технологии

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import Promo from "../Promo/Promo"
+import AboutProject from "../AboutProject/AboutProject"
+import Techs from "../Techs/Techs"
 
 export default function Main() {
   return (
     <>
       <main>
         <Promo />
+        <AboutProject />
+        <Techs />
       </main>
     </>
   )
