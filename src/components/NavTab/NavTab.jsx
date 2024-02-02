9c3b941661e1b90c1a676203b19717975e7bc02d
psychoHAHA <80909413+psychoHAHA@users.noreply.github.com>
2024-02-02 17:46:09 +0300

    кнопки о проекте

---
+++ src/components/NavTab/NavTab.jsx
@@ -0,0 +1,13 @@
+import "./NavTab.css"
+
+export default function NavTab() {
+  return (
+    <>
+      <nav className="nav">
+        <a href="#" className="nav__link">О проекте</a>
+        <a href="#" className="nav__link">Технологии</a>
+        <a href="#" className="nav__link">Студент</a>
+      </nav>
+    </>
+  )
+}
