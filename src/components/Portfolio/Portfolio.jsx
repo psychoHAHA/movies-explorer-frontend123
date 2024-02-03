4abab172b0daaf4ab933fd68aadf69c14abfd6c3
psychoHAHA <80909413+psychoHAHA@users.noreply.github.com>
2024-02-03 20:22:10 +0300

    главная страница готова!

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,36 +5,39 @@ export default function Portfolio() {
   return (
     <>
       <div className="portfolio">
-      <h4 className="portfolio__title">Портфолио</h4>
+        <h3 className="portfolio__title">Портфолио</h3>
         <ul className="portfolio__links">
           <li className="portfolio__links-type_item">
-            <a href="" className="portfolio__link">
+            <a
+              href="https://github.com/psychoHAHA/practica1"
+              target="_blank"
+              className="portfolio__link"
+              rel="noreferrer"
+            >
               Статичный сайт
-              <img
-                src={linkImage}
-                alt=""
-                className="portfolio__link-image"
-              />
+              <img src={linkImage} alt="" className="portfolio__link-image" />
             </a>
           </li>
           <li className="portfolio__links-type_item">
-            <a href="" className="portfolio__link">
+            <a
+              href="https://psychohaha.github.io/russian-travel/"
+              target="_blank"
+              className="portfolio__link"
+              rel="noreferrer"
+            >
               Адаптивный сайт
-              <img
-                src={linkImage}
-                alt=""
-                className="portfolio__link-image"
-              />
+              <img src={linkImage} alt="" className="portfolio__link-image" />
             </a>
           </li>
           <li className="portfolio__links-type_item">
-            <a href="" className="portfolio__link">
+            <a
+              href="https://psychohaha.github.io/mesto-react/"
+              target="_blank"
+              className="portfolio__link"
+              rel="noreferrer"
+            >
               Одностраничное приложение
-              <img
-                src={linkImage}
-                alt=""
-                className="portfolio__link-image"
-              />
+              <img src={linkImage} alt="" className="portfolio__link-image" />
             </a>
           </li>
         </ul>
