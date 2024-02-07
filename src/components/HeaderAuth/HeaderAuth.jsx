e98eee145ce360dcb5c4eff0b974cddcf75d9f22
psychoHAHA <80909413+psychoHAHA@users.noreply.github.com>
2024-02-07 19:12:35 +0300

    burger полностью готов

--- src/components/HeaderAuth/HeaderAuth.jsx
+++ src/components/HeaderAuth/HeaderAuth.jsx
@@ -5,34 +5,48 @@ import "./HeaderAuth.css"
 
 export default function HeaderAuth() {
   const location = useLocation()
-  const [ isOpen, setIsOpen] = useState()
+  const [isOpen, setIsOpen] = useState()
 
   return (
     <>
-      <ul className={`header__navbar ${isOpen ? "header__navbar--active" : ""}`} >
-        <li className="header__navbar-item">
-          <NavLink to="/" className="header__navbar-item_type_link">
-            Главная
-          </NavLink>
-        </li>
-        <li className="header__navbar-item">
-          <NavLink to="/movies" className="header__navbar-item_type_link">
-            Фильмы
-          </NavLink>
-        </li>
-        <li className="header__navbar-item">
-          <NavLink to="/saved-movies" className="header__navbar-item_type_link">
-            Сохраненные фильмы
-          </NavLink>
-        </li>
-      </ul>
-      <Link to="/profile">
-        <button className="header__button header__button-profile" type="button">
-          Аккаунт
-        </button>
-      </Link>
+      <nav
+        className={`header__navbar ${isOpen ? "header__navbar--active" : ""}`}
+      >
+        <ul className="header__navbar-list">
+          <li className="header__navbar-item">
+            <NavLink to="/" className="header__navbar-item_type_link header__navbar-item_type_main" >
+              Главная
+            </NavLink>
+          </li>
+          <li className="header__navbar-item">
+            <NavLink to="/movies" className="header__navbar-item_type_link">
+              Фильмы
+            </NavLink>
+          </li>
+          <li className="header__navbar-item">
+            <NavLink
+              to="/saved-movies"
+              className="header__navbar-item_type_link"
+            >
+              Сохраненные фильмы
+            </NavLink>
+          </li>
+        </ul>
 
-      <button className={`header__burger ${isOpen ? "header__burger--active" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+        <Link to="/profile">
+          <button
+            className="header__button header__button-profile"
+            type="button"
+          >
+            Аккаунт
+          </button>
+        </Link>
+      </nav>
+
+      <button
+        className={`header__burger ${isOpen ? "header__burger--active" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <span className="header__burger-line "></span>
         <span className="header__burger-line"></span>
         <span className="header__burger-line"></span>
