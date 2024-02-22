166d4470efbeaf95ff6b6716d62af7f2d7c0d66c
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-02-22 21:54:25 +0500

    123

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -9,8 +9,8 @@ export default function MoviesCard() {
     <>
       <li className="movies-card">
         <a
-          href="https://ru.stackoverflow.com/questions/794414/%D0%9A%D0%BD%D0%BE%D0%BF%D0%BA%D0%B0-%D0%BF%D0%BE%D0%B2%D0%B5%D1%80%D1%85-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F-%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B8"
-          className="movies-card__link"
+          href="#"
+          className="movies-cardBD__link"
           target="_blank"
           rel="noreferrer"
         >
