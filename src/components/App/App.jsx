b230d8fe1ab62313bf4f68ac307400a753a639ed
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-02-28 15:29:28 +0500

    123

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from "react"
-import { Route, Routes, Navigate, useNavigate } from "react-router-dom"
-import { CurrentUserContext } from "../../contexts/CurrentUserContext"
+import React, { useState, useEffect } from 'react'
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom'
+import { CurrentUserContext } from '../../contexts/CurrentUserContext'
 
-import * as mainApi from "../../utils/MainApi.js"
+import * as mainApi from '../../utils/MainApi.js'
 
-import "./App.css"
+import './App.css'
 
-import Main from "../Main/Main"
-import ProtectedRoute from "../ProtectedRoute"
-import Register from "../Register/Register"
-import Login from "../Login/Login"
-import Movies from "../Movies/Movies"
-import Profile from "../Profile/Profile"
-import SavedMovies from "../SavedMovies/SavedMovies"
-import ErrorNotFound from "../ErrorNotFound/ErrorNotFound"
+import Main from '../Main/Main'
+import ProtectedRoute from '../ProtectedRoute'
+import Register from '../Register/Register'
+import Login from '../Login/Login'
+import Movies from '../Movies/Movies'
+import Profile from '../Profile/Profile'
+import SavedMovies from '../SavedMovies/SavedMovies'
+import ErrorNotFound from '../ErrorNotFound/ErrorNotFound'
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState()
   const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState('')
   const [setFormValue] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   })
 
   const navigate = useNavigate()
@@ -30,10 +30,33 @@ export default function App() {
     mainApi
       .register(name, email, password)
       .then(() => {
-        navigate("/signin", { replace: true })
+        navigate('/sign-in', { replace: true });
       })
-      .catch((err) => console.log(err))
-  }
+
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const checkToken = () => {
+    const token = localStorage.getItem('jwt');
+
+    if (token) {
+      mainApi
+        .getContent(token)
+        .then((res) => {
+          setLoggedIn(true);
+          setEmail(res.data.email);
+          navigate('/', { replace: true });
+        })
+
+        .catch((err) => console.log(err));
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
@@ -41,7 +64,11 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<Main />}></Route>
 
-          <Route exact path="/signup" element={<Register />}></Route>
+          <Route
+            exact
+            path="/signup"
+            element={<Register onRegister={handleRegister} />}
+          ></Route>
           <Route exact path="/signin" element={<Login />}></Route>
           <Route
             exact
